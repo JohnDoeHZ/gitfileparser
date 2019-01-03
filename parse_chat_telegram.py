ed37836e84d3d7c709fc36d899634d980b89c288
JohnDoeHZ <34421207+JohnDoeHZ@users.noreply.github.com>
2019-01-03 16:33:33 +0700

    Create parse_chat_telegram.py

---
+++ parse_chat_telegram.py
@@ -0,0 +1,23 @@
+from pyquery import PyQuery  
+from collections import Counter
+import pandas as pd
+
+	
+data = []
+for i in range(1,78):
+	print('Parsing a file - messages'+str(i)+'.html')
+	f = open('chat/messages'+str(i)+'.html',encoding='utf-8')
+	doc = PyQuery(f.read())
+	body = doc('div[class=body]')
+	author = doc('div[class=from_name]').remove('span[class=details]')
+
+	for elem in author.items():
+		data.append(elem.text())
+	f.close()
+
+df = pd.DataFrame.from_dict(Counter(data), orient='index').reset_index()
+df = df.rename(columns={'index':'name', 0:'count'})
+df = df.sort_values('count',ascending=False)
+df.index.name = 'index'
+print(df)
+df.to_csv('csv.csv',index=False)
